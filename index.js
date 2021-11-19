1cc727e617dc574f395164714b3842ad013db0c0
XeKkoW <gabriel.vigne@edu.ece.fr>
2021-11-19 12:32:09 +0100

    test

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-"console.log('Hello World')"  
+const express = require('express');
+
+const hostname = 'loacalhost'
+const port = 3000;
+
+const app = express();
+
+app.listen(port, hostname, () => {
+	console.log(`Mon serveur fonctionne sur http://${hostname}:${port}\n`);
+});
+
+app.get('/test', (req, res) => {
+	res.json({
+		req: req.method,
+		data: "This is a GET"
+	})
+});
+
+app.put('/test', (req, res) => {
+	res.json({
+		req: req.method,
+		data: "This is a PUT"
+	});
+});
+
+app.listen(port, hostname, () => {
+	console.log('App listening on port 3000!')
+});
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
+});
